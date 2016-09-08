277a000ad2fc4632dd37738a0f78dab67de74a13
Marius Raducan <marius.raducan@softvision.ro>
2016-09-08 12:31:10 +0300

    Initial Commit

---
+++ FastlaneTechTalk/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FastlaneTechTalk
+//
+//  Created by Marius Raducan on 08/09/16.
+//  Copyright © 2016 Softvision. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
