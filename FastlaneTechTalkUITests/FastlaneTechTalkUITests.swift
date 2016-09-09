5f3602b59b8cc889950f2f47e67e5703217978c0
Marius Raducan <marius.raducan@softvision.ro>
2016-09-09 17:11:53 +0300

    fastlane configs

--- FastlaneTechTalkUITests/FastlaneTechTalkUITests.swift
+++ FastlaneTechTalkUITests/FastlaneTechTalkUITests.swift
@@ -18,7 +18,9 @@ class FastlaneTechTalkUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -30,6 +32,21 @@ class FastlaneTechTalkUITests: XCTestCase {
     
     func testExample() {
         // Use recording to get started writing UI tests.
+        snapshot("first")
+        XCUIDevice.sharedDevice().orientation = .Portrait
+        XCUIDevice.sharedDevice().orientation = .LandscapeRight
+        snapshot("firstLandscapeRight")
+        XCUIDevice.sharedDevice().orientation = .PortraitUpsideDown
+        XCUIDevice.sharedDevice().orientation = .LandscapeLeft
+        XCUIDevice.sharedDevice().orientation = .Portrait
+        
+        let tabBarsQuery = XCUIApplication().tabBars
+        tabBarsQuery.buttons["Second"].tap()
+        snapshot("second")
+        XCUIDevice.sharedDevice().orientation = .LandscapeRight
+        snapshot("secondLandscape")
+        XCUIDevice.sharedDevice().orientation = .Portrait
+        tabBarsQuery.buttons["First"].tap()
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
